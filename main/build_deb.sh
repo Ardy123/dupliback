4ede2a3788fac4b5630d3fc1d178510fcd1fe07c
ArdyFalls <ArdyFalls@03a8ba48-08cc-11df-9a7a-53bbb444ad82>
2010-01-24 11:05:42 +0000

    Moved the basic packaging stuff to a packaging directory

--- main/build_deb.sh
+++ main/build_deb.sh
@@ -3,8 +3,21 @@ VERSION="`python src/settings.py`"
 VERSION="${VERSION}_r`svnversion .`"
 echo $VERSION
 rm  debs/*
-cp  src/*.pyc debian/usr/share/dupliback
-cp  src/flyback.py debian/usr/share/dupliback
+# make necessary directories
+mkdir debian
+mkdir debian/DEBIAN
+mkdir debian/usr/
+mkdir debian/usr/share
+mkdir debian/usr/share/dupliback
+mkdir debian/usr/share/dupliback/glade
+mkdir debian/usr/share/applications
+# copy files over
+cp src/*.pyc debian/usr/share/dupliback
+cp src/flyback.py debian/usr/share/dupliback
+cp src/glade/*.glade debian/usr/share/dupliback/glade/
+cp packaging/control debian/DEBIAN
+cp packaging/dupliback.desktop debian/usr/share/applications/
+# build package
 dpkg-deb --build debian
 mv debian.deb debs/dupli.back-$VERSION.deb
 
