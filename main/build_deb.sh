0969df9cc4459c8f1544733e5889994bc65f3d46
ArdyFalls <ArdyFalls@03a8ba48-08cc-11df-9a7a-53bbb444ad82>
2010-01-27 09:11:10 +0000

    Removed references to flyback, issue 2 Fixed bug in deb build script that was causing issue 6 and issue 7

--- main/build_deb.sh
+++ main/build_deb.sh
@@ -12,8 +12,7 @@ mkdir debian/usr/share/dupliback
 mkdir debian/usr/share/dupliback/glade
 mkdir debian/usr/share/applications
 # copy files over
-cp src/*.pyc debian/usr/share/dupliback
-cp src/flyback.py debian/usr/share/dupliback
+cp src/*.py debian/usr/share/dupliback
 cp src/glade/*.glade debian/usr/share/dupliback/glade/
 cp packaging/control debian/DEBIAN
 cp packaging/dupliback.desktop debian/usr/share/applications/
